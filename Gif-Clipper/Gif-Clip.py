ccb90940c5feec4dbd6c8d1d4d883db8b5c0b683
Aaron Gill <arg12@sfu.ca>
2022-10-19 22:37:27 -0700

    Figured out how to search subfile using pysrt

--- Gif-Clipper/Gif-Clip.py
+++ Gif-Clipper/Gif-Clip.py
@@ -1,1 +1,16 @@
 #using python to get the initial prototype working
+import pysrt
+from re import search
+subs = pysrt.open('2_English.srt')
+substring = "job"
+
+#print(subs[0])
+for sub in subs:
+    if search(substring,sub.text):
+        print(sub.text)
+        print("\nStart Time: " + str(sub.start.minutes) + ":" + str(sub.start.seconds) + "," + str(sub.start.milliseconds))
+        print("End Time: " + str(sub.end.minutes) + ":" + str(sub.end.seconds) + "," + str(sub.end.milliseconds) +"\n")
+        #print(sub.start.milliseconds)
+        #print(sub.end.minutes)
+        #print(sub.end.seconds)
+        #print(sub.end.milliseconds)
